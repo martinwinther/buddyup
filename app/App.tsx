a422f9e5e7831df774b61f06171fa2731ae0b2d3
Martin Winther <martin.winther@protonmail.com>
2025-10-13 06:45:38 +0200

    add client and test query

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { supabase } from './src/lib/supabase';
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase.from('categories').select('*');
+      console.log('categories', data, error);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
