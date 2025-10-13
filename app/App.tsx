e9756f3336accaeab15cf498f256c82217937408
Martin Winther <martin.winther@protonmail.com>
2025-10-13 07:33:02 +0200

    remove unused global CSS import and nativewind plugin from babel config

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,3 @@
-import '../global.css';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Navigation from './src/navigation';
